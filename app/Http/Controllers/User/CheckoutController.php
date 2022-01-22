2286a275b3225724687ccd2d8be74f904634559b
rahulken96 <rahulken96@gmail.com>
2022-01-22 13:36:37 +0700

    Integrasi Checkout

---
+++ app/Http/Controllers/User/CheckoutController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Camp;
+use App\Models\Checkout;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\VarDumper\VarDumper;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Camp $camp)
+    {
+        return view('checkout',['camp' => $camp]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Camp $camp)
+    {
+        //Mapping request data
+        $data = $request->all();
+        $data['user_id'] = Auth::id();
+        $data['camp_id'] = $camp->id;
+        $data['expired'] = date('Y-m-t', strtotime($data['expired']));
+
+        //Update user data
+        $user = Auth::user();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->occupation = $data['occupation'];
+        $user->save();
+
+        //create checkout data
+        $checkout = Checkout::create($data);
+
+        return redirect(route('checkout_sukses'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Checkout  $checkout
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Checkout  $checkout
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Checkout  $checkout
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Checkout  $checkout
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Checkout $checkout)
+    {
+        //
+    }
+
+    public function sukses()
+    {
+        return view('success_checkout');
+    }
+}
